83c880e49f3224f0bc212c63d007d6e8c120fe1e
Priya Garg <priya1070.be21@chitkara.edu.in>
2023-08-28 09:46:09 +0530

    after updation

---
+++ post.js
@@ -0,0 +1,60 @@
+let form=document.querySelector(".post-form");
+let input=document.querySelector(".textinp");
+let image=document.querySelector(".imginp")
+let button=document.querySelector(".postbtn")
+let container=document.querySelector(".cont");
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+    let data=input.value
+    let img=image.value
+    console.log(data);
+    addPost(data,img);
+    input.value=" ";
+    image.value=" ";
+
+
+
+    
+
+});
+function addPost(data,img){
+    let post=document.createElement("div");
+    post.classList.add("post");
+    post.innerHTML=`<div class="img-container">
+    <img src="https://th.bing.com/th/id/R.3a9f0210ff7d1e83039ff237c81bacae?rik=asArcL6qvWfZfA&riu=http%3a%2f%2fs1.picswalls.com%2fwallpapers%2f2016%2f03%2f29%2fbeautiful-nature-high-definition_042323787_304.jpg&ehk=8tO7UxFL%2bC03x6vfc2O9EhdUCe4fl6tC7UGUdlnpdX4%3d&risl=&pid=ImgRaw&r=0" alt="">
+        </div>
+        <div class="details">
+            <p class="handle"><strong>@magicalworld</strong></p>
+         
+        </div>
+
+    
+    
+    <p class="postext">${data}</p>
+    <div class="post-img">
+    
+    <img src="${img}">
+    <p class="sub-text">3 minutes ago..</p>
+    </div>
+    <div class="btn-container">
+    <div class="edit"><svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.239 15.533c-1.045 3.004-1.238 3.451-1.238 3.84 0 .441.385.627.627.627.272 0 1.108-.301 3.829-1.249zm.888-.888 3.22 3.22 6.408-6.401c.163-.163.245-.376.245-.591 0-.213-.082-.427-.245-.591-.58-.579-1.458-1.457-2.039-2.036-.163-.163-.377-.245-.591-.245-.213 0-.428.082-.592.245zm-3.127-.895c0-.402-.356-.75-.75-.75-2.561 0-2.939 0-5.5 0-.394 0-.75.348-.75.75s.356.75.75.75h5.5c.394 0 .75-.348.75-.75zm5-3c0-.402-.356-.75-.75-.75-2.561 0-7.939 0-10.5 0-.394 0-.75.348-.75.75s.356.75.75.75h10.5c.394 0 .75-.348.75-.75zm0-3c0-.402-.356-.75-.75-.75-2.561 0-7.939 0-10.5 0-.394 0-.75.348-.75.75s.356.75.75.75h10.5c.394 0 .75-.348.75-.75zm0-3c0-.402-.356-.75-.75-.75-2.561 0-7.939 0-10.5 0-.394 0-.75.348-.75.75s.356.75.75.75h10.5c.394 0 .75-.348.75-.75z" fill-rule="nonzero"/></svg></div>
+             <div class="delete"><svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12.002 2.005c5.518 0 9.998 4.48 9.998 9.997 0 5.518-4.48 9.998-9.998 9.998-5.517 0-9.997-4.48-9.997-9.998 0-5.517 4.48-9.997 9.997-9.997zm0 8.933-2.721-2.722c-.146-.146-.339-.219-.531-.219-.404 0-.75.324-.75.749 0 .193.073.384.219.531l2.722 2.722-2.728 2.728c-.147.147-.22.34-.22.531 0 .427.35.75.751.75.192 0 .384-.073.53-.219l2.728-2.728 2.729 2.728c.146.146.338.219.53.219.401 0 .75-.323.75-.75 0-.191-.073-.384-.22-.531l-2.727-2.728 2.717-2.717c.146-.147.219-.338.219-.531 0-.425-.346-.75-.75-.75-.192 0-.385.073-.531.22z" fill-rule="nonzero"/></svg></div>
+                
+    </div>`
+container.prepend(post);
+
+}
+container.addEventListener("click",function(ev)
+{
+   
+    let ele=ev.target.parentElement.parentElement;
+    let classname=ele.classList;
+     console.log(ele.className)
+    // console.log(ev)
+
+    if(ele.className="delete"){
+        ele.parentElement.parentElement.remove();
+       
+    }
+    
+})
